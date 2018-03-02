328d9f786d9438481611befb2f83a4ac848c6c1a
timothy19870826 <timothy19870826@163.com>
2018-03-02 09:31:51 +0800

    lalala

--- WebContent/js/bcnew/bcnewEntity.js
+++ WebContent/js/bcnew/bcnewEntity.js
@@ -6,10 +6,28 @@ define(function (){
 	
 	function Entity(name){
 		this.name = name | "entity";
+		this.active = true;
+		this.dead = false;
+		this.parent = null;
+		this.children = new Array();
 	};
 	
 	Entity.prototype.getName = function(){
 		return this.name;
+	}
+	
+	Entity.prototype.addChild = function(entity){
+		if (this.children.indexOf(entity) == -1){
+			entity.parent = null;
+			this.children.push(entity);
+		}
+	}
+	
+	Entity.prototype.removeChild = function(entity) {
+		var idx = this.children.indexOf(entity);
+		if (idx >= 0){
+			entity.dead = true;
+		}
 	}
 
 	Entity.prototype.init = function(){
@@ -17,11 +35,32 @@ define(function (){
 	};
 	
 	Entity.prototype.update = function(){
-		
+		if (this.children.length == 0){
+			return;
+		}
+		for (var idx = this.children.length - 1; idx >= 0; --idx){
+			if (this.children[idx].dead){
+				this.children.splice(idx, 1);
+			}
+			else if (this.children[idx].active){
+				this.children[idx].update();
+			}
+		}
 	};
 	
 	Entity.prototype.lateUpdate = function(){
-		
+		if (this.children.length == 0){
+			return;
+		}
+		for (var idx = this.children.length - 1; idx >= 0; --idx){
+			if (this.children[idx].dead){
+				this.children[idx].parent = null;
+				this.children.splice(idx, 1);
+			}
+			else if (this.children[idx].active){
+				this.children[idx].lateUpdate();
+			}
+		}
 	};
 	
 	Entity.prototype.destroy = function(){
